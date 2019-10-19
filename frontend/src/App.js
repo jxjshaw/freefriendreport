6ba80fc33392c52b2cb02700c08b28a3b5d6f922
jxjshaw <jxjshaw@gmail.com>
2019-10-19 16:00:56 -0700

    add card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './smarticon.gif';
 import './App.css';
+import FriendCard from './FriendCard.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         
-        <h1>Freefriendsreport.com</h1>
+        <h1>Freefriendsreport.com <img src={logo} width="80" height="" className="App-logo" alt="logo" /></h1>
         <h4>The ONLY free source for finding out who your real friends are</h4>
-        <img src={logo} width="150" height="" className="App-logo" alt="logo" />
-       
-      </header>
+        
+
+
+        <FriendCard/>
+      
     </div>
   );
 }
