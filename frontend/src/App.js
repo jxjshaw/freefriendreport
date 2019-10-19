076d457c0d55ecbc9820332e88ef2872686aaa79
jxjshaw <jxjshaw@gmail.com>
2019-10-19 15:00:38 -0700

    Adding create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './smarticon.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>FIND OUT WHO YOUR REAL FRIENDS ARE</h1>
+        <img src={logo} width="150" className="App-logo" alt="logo" />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
