7ca17b0b36f1d970937ea7b58b898c34762478fe
jxjshaw <jxjshaw@gmail.com>
2019-10-20 12:42:15 -0700

    add instruction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './smarticon.gif';
 import './App.css';
-import FriendCard from './FriendCard.js'
+import FriendCard from './FriendCard.js';
+import Instructions from './Instructions.js';
 import FRIENDREPORTJSON from './data.json'
 
 
@@ -46,29 +47,34 @@ function App() {
 
   return (
     <div className="App">
-        
-        <h1>Freefriendsreport.com <img src={logo} width="80" height="" className="App-logo" alt="logo" /></h1>
-        <h4>The ONLY free source for finding out who your real friends are</h4>
+        <div>         
+            <h1>Freefriendsreport.com <img src={logo} width="80" height="" className="App-logo" alt="logo" /></h1>
+            <h4>The ONLY free source for finding out who your real friends are</h4>
 
-        <p>
-        The Centre for Digitally Transmitted Relationships <br/><br/>
+            <p>
+            The Centre for Digitally Transmitted Relationships <br/><br/>
 
-        Ever been left on read? <br/>
-        Ever felt like you reached out more than they did? <br/>
-        Ever been the person to not text back? <br/>
+            Ever been left on read? <br/>
+            Ever felt like you reached out more than they did? <br/>
+            Ever been the person to not text back? <br/>
 
-        Time to expose your text based relationships.  <br/>
+            Time to expose your text based relationships.  <br/>
 
-        You've been here. <br/>
+            You've been here. <br/>
 
-        This is you, in blue. <br/>
+            This is you, in blue. <br/>
 
-        And this is your friend in grey. <br/>
+            And this is your friend in grey. <br/>
 
-        Our service allows you to see all your messaging history in an accurate stream of blue to grey ratio to see who initiated first, who initiated more, who lost interest <br/>
-        </p>
-        
-        Your TOTAL score = aka the aggregate
+            Our service allows you to see all your messaging history in an accurate stream of blue to grey ratio to see who initiated first, who initiated more, who lost interest <br/>
+            </p>
+            
+            Your TOTAL score = aka the aggregate
+        </div>
+
+        <Instructions/>
+
+    
         {listItems}
         
         <form action="https://www.facebook.com/dyi?x=Adm94qWFKLgs1TQe">
